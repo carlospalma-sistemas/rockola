c2a0dc2c8d4b67578e47355a2a8ea4fd336ba7c1
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-09-06 18:38:04 -0500

    Creada versión BD 20220906

---
+++ databases/rockola_20220906.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rockola
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tcanciones`
+--
+
+DROP TABLE IF EXISTS `tcanciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tcanciones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `aniopublicacion` int NOT NULL,
+  `compositor` varchar(70) NOT NULL,
+  `interprete` varchar(70) NOT NULL,
+  `imagen` varchar(45) NOT NULL,
+  `id_generomusical` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `imagen_UNIQUE` (`imagen`),
+  KEY `fkgenerodecancion_idx` (`id_generomusical`),
+  CONSTRAINT `fkgenerodecancion` FOREIGN KEY (`id_generomusical`) REFERENCES `tgenerosmusicales` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tcanciones`
+--
+
+LOCK TABLES `tcanciones` WRITE;
+/*!40000 ALTER TABLE `tcanciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tcanciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tgenerosmusicales`
+--
+
+DROP TABLE IF EXISTS `tgenerosmusicales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tgenerosmusicales` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `carpeta` varchar(45) NOT NULL,
+  `hab` tinyint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  UNIQUE KEY `carpeta_UNIQUE` (`carpeta`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tgenerosmusicales`
+--
+
+LOCK TABLES `tgenerosmusicales` WRITE;
+/*!40000 ALTER TABLE `tgenerosmusicales` DISABLE KEYS */;
+INSERT INTO `tgenerosmusicales` VALUES (1,'Rock en español','rock',1),(3,'Salsa','salsa',1),(5,'Cumbia','cumbias',1),(6,'Merengue','merengue',1);
+/*!40000 ALTER TABLE `tgenerosmusicales` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-06 18:35:01
